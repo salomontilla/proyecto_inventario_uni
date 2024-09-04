86bc26af07a4946651fd8f59a1eb936574fa46ca
salo <salomontilla@outlook.com>
2024-09-03 22:56:39 -0500

    agrega archivos

---
+++ b_montilla_salomon_parcial_1.py
@@ -0,0 +1,17 @@
+'''
+PROBLEMA:
+----------
+La aplicación permite gestionar un inventario de ingredientes, 
+calcular el costo de los platos en función 
+de los ingredientes utilizados, y determinar el 
+precio ideal de venta para maximizar las ganancias.
+----------
+LENGUAJE A UTILIZAR:
+----------
+
+----------
+PLANIFICACION:
+----------
+
+----------
+'''
