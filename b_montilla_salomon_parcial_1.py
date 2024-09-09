e566680caf9cc56c09f03ed0eca759def68a9e2e
universidad <salomon.montilla.l@uniautonoma.edu.co>
2024-09-09 09:24:58 -0500

    cambiando nombnres de ingredientes a platos

--- b_montilla_salomon_parcial_1.py
+++ b_montilla_salomon_parcial_1.py
@@ -1,62 +1,35 @@
 '''
 PROBLEMA:
 ----------
-La aplicación permite gestionar un inventario de ingredientes, 
+La aplicación permite gestionar un inventario de platos, 
 calcular el costo de los platos en función 
-de los ingredientes utilizados, y determinar el 
+de los platos utilizados, y determinar el 
 precio ideal de venta para maximizar las ganancias.
 ----------
 TECNOLOGIAS A UTILIZAR:
 ----------
 - Python
-- Django
-- Sqlite
 ----------
 PLANIFICACION:
 ----------
-Gestión de Ingredientes:
-1. Agregar Ingredientes: El usuario puede añadir nuevos 
-ingredientes al inventario con sus costos respectivos.
+Gestión de platos:
+1. Agregar platos: El usuario puede añadir nuevos 
+platos al inventario con sus costos respectivos.
 
-2. Actualizar Ingredientes: Modificar la información de 
+2. Actualizar platos: Modificar la información de 
 un ingrediente existente (precio, cantidad, etc.).
 
-3. Eliminar Ingredientes: Remover ingredientes que ya no 
+3. Eliminar platos: Remover platos que ya no 
 se utilizan.
 
 Cálculo de Costos y Precios:
 1. Calcular Costo de Plato: Determinar el costo total de un 
-plato basado en la receta y los precios de los ingredientes.
+plato basado en la receta y los precios de los platos.
 
 2. Establecer Precio de Venta: Proponer un precio de venta para el plato basado 
 en el costo y el margen de ganancia deseado.
 ----------
 '''
-<<<<<<< HEAD
-#funcion que agrega ingredientes y retorna la lista de ingredientes agregados
-def agregarIngrediente ():
-    ingredientes = []
-
-    salir = 1
-    while salir != 0:
-        nombre_ingre= input("Ingresa el nombre del ingrediente para agregar: ")
-        precio = float(input("Ingresa el precio del ingrediente: "))   
-        ingre = {
-        "nombre" : nombre_ingre,
-        "precio" : precio
-        }
-        ingredientes.append(ingre)
-        print("Producto Agregado!")
-
-        sal = int(input("Presiona 1 para agregar otro ingrediente, 0 para salir: "))
-        if(sal == 0):
-            salir = 0
-    return ingredientes
-
-
-if __name__ == "__main__":
-    print(agregarIngrediente())    
-=======
 #Esta funcion despliega el menú de opciones
 def display_menu():
     salir = 0
@@ -114,4 +87,3 @@ def modificar():
     print("modificar")
 if __name__ == "__main__":
     display_menu()
->>>>>>> a0289126079f1c8568e28b1e83281e2a880da0e4
